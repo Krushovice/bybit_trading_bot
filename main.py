4a19621d30a3476f98acefdb3fed2cb37a9f66bf
Krushovice <kickstar69@yandex.ru>
2024-08-26 21:04:25 +1000

    add try/except

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
+from pybit import exceptions
 from pybit.unified_trading import HTTP
+
 
 logging.basicConfig(
     format="%(asctime)s %(message)s",
@@ -8,10 +10,16 @@ logging.basicConfig(
 
 
 def main():
-    client = HTTP()
-    response = client.get_orderbook(category="linear", symbol="BTCUSDT")
-
-    print(response)
+    client = HTTP(
+        testnet=True,
+        recv_window=60000,
+        max_retries=3,
+    )
+    try:
+        response = client.get_orderbook(category="linear", symbol="BTCUSDT")
+        print(response)
+    except exceptions.InvalidRequestError as e:
+        print(f"Ошибка запроса: {e.status_code}| {e.message}")
 
 
 if __name__ == "__main__":
